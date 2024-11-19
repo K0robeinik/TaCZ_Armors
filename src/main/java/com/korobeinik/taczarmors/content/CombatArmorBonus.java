67d3c90f049ed9bc8c017ae046689c7544084126
K0robeinik <danivish2005@gmail.com>
2024-11-19 16:31:08 +0200

    Backup before reworking Armor Materials

--- src/main/java/com/korobeinik/taczarmors/content/CombatArmorBonus.java
+++ src/main/java/com/korobeinik/taczarmors/content/CombatArmorBonus.java
@@ -2,7 +2,11 @@ package com.korobeinik.taczarmors.content;
 
 public enum CombatArmorBonus {
     SPEED,
+    SWIM_SPEED,
     HEALTH,
     JUMPHEIGHT,
-    FALLHEIGHT
+    FALLHEIGHT,
+    ATTACK_DAMAGE,
+    ATTACK_SPEED,
+    ATTACK_KNOCKBACK
 }
--- src/main/java/com/korobeinik/taczarmors/content/CombatArmorMaterials.java
+++ src/main/java/com/korobeinik/taczarmors/content/CombatArmorMaterials.java
@@ -1,6 +1,7 @@
 package com.korobeinik.taczarmors.content;
 
 import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ArmorItem;
@@ -25,13 +26,13 @@ public class CombatArmorMaterials implements CombatArmorMaterial{
     public float knockbackResistance;
     private final float[] hpBonus;
     private final float[] speed;
-    private final float[] swimSpeed;
     private final float[] jumpHeight;
     private final float stepHeight;
     private final float[] fallHeight;
+    private final float[] swimSpeed;
+    private final float[] attackDamage;
     private final float[] attackSpeed;
     private final float[] attackKnockback;
-    private final float[] attackDamage;
 
     public CombatArmorMaterials(String name, int durability, int[] damageReduction, int encantability, SoundEvent sound, Ingredient ingredient, float toughness) {
         this(name, durability, damageReduction, encantability, sound, ingredient, toughness, ZERO_ARRAY, ZERO_ARRAY, 0F, ZERO_ARRAY, ZERO_ARRAY, 0F, ZERO_ARRAY);
@@ -75,7 +76,7 @@ public class CombatArmorMaterials implements CombatArmorMaterial{
     public int getEnchantmentValue() { return this.encantability; }
 
     @Override
-    public @NotNull SoundEvent getEquipSound() { return this.sound; }
+    public @NotNull SoundEvent getEquipSound() { return this.sound == null ? SoundEvents.EMPTY : this.sound; }
 
     @Override
     public @NotNull Ingredient getRepairIngredient() { return this.ingredient == null ? Ingredient.EMPTY : this.ingredient; }
@@ -93,9 +94,13 @@ public class CombatArmorMaterials implements CombatArmorMaterial{
     public float getBonusForType(CombatArmorBonus bonus, ArmorItem.Type type) {
         switch (bonus){
             case SPEED -> {return speed[type.ordinal()];}
+            case SWIM_SPEED -> {return swimSpeed[type.ordinal()];}
             case HEALTH -> {return hpBonus[type.ordinal()];}
             case JUMPHEIGHT -> {return jumpHeight[type.ordinal()];}
             case FALLHEIGHT -> {return fallHeight[type.ordinal()];}
+            case ATTACK_DAMAGE -> {return attackDamage[type.ordinal()];}
+            case ATTACK_SPEED -> {return attackSpeed[type.ordinal()];}
+            case ATTACK_KNOCKBACK -> {return attackKnockback[type.ordinal()];}
         }
         return 0;
     }
