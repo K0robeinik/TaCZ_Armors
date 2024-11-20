733292614b1f8d3a218066cb4bdf101b565673ad
K0robeinik <danivish2005@gmail.com>
2024-11-20 08:45:57 +0200

    Horse Armor is now colored in inventory

--- src/main/java/com/korobeinik/taczarmors/content/KevlarHorseArmorItem.java
+++ src/main/java/com/korobeinik/taczarmors/content/KevlarHorseArmorItem.java
@@ -1,8 +1,10 @@
 package com.korobeinik.taczarmors.content;
 
 import com.korobeinik.taczarmors.TaczArmors;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.DyeableHorseArmorItem;
+import net.minecraft.world.item.ItemStack;
 
 public class KevlarHorseArmorItem extends DyeableHorseArmorItem {
     public KevlarHorseArmorItem(int pProtection, String pIdentifier, Properties pProperties) {
@@ -11,5 +13,14 @@ public class KevlarHorseArmorItem extends DyeableHorseArmorItem {
 
     public KevlarHorseArmorItem(int pProtection, ResourceLocation pIdentifier, Properties pProperties) {
         super(pProtection, pIdentifier, pProperties);
+    }
+
+    public int getColor(ItemStack pStack) {
+        CompoundTag compoundtag = pStack.getTagElement("display");
+        return compoundtag != null && compoundtag.contains("color", 99) ? compoundtag.getInt("color") : 8421376;
+    }
+
+    public static int getItemColor(ItemStack stack, int ignoredTint){
+        return ((KevlarHorseArmorItem) stack.getItem()).getColor(stack);
     }
 }
--- src/main/java/com/korobeinik/taczarmors/events/ModClientEvents.java
+++ src/main/java/com/korobeinik/taczarmors/events/ModClientEvents.java
@@ -2,6 +2,7 @@ package com.korobeinik.taczarmors.events;
 
 import com.korobeinik.taczarmors.TaczArmors;
 import com.korobeinik.taczarmors.content.ColorBottle;
+import com.korobeinik.taczarmors.content.KevlarHorseArmorItem;
 import com.korobeinik.taczarmors.init.ItemInit;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.RegisterColorHandlersEvent;
@@ -14,6 +15,7 @@ public class ModClientEvents {
     //@OnlyIn(Dist.CLIENT)
     public static void onRegisterItemColor(RegisterColorHandlersEvent.Item event){
         //event.register(((stack, tint) -> tint > 0 ? 0xFFFFFF : ColorBottle.getDyeColor(stack)), ItemInit.COLOR_BOTTLE.get());
-        event.register((ColorBottle::getItemColor), ItemInit.COLOR_BOTTLE.get());
+        event.register(ColorBottle::getItemColor, ItemInit.COLOR_BOTTLE.get());
+        event.register(KevlarHorseArmorItem::getItemColor, ItemInit.KEVLAR_HORSE_ARMOR.get());
     }
 }
