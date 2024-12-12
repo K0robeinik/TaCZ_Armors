7b19a5d2ca3cea8b33f07cc9b7c7c4f7563e5ca3
K0robeinik <danivish2005@gmail.com>
2024-12-12 08:36:04 +0200

    Added Shake Sensitivity

--- src/main/java/com/korobeinik/taczarmors/client/overlays/HelmetOverlay.java
+++ src/main/java/com/korobeinik/taczarmors/client/overlays/HelmetOverlay.java
@@ -3,17 +3,14 @@ package com.korobeinik.taczarmors.client.overlays;
 import com.korobeinik.taczarmors.TaczArmors;
 import com.korobeinik.taczarmors.config.ClientConfig;
 import com.korobeinik.taczarmors.items.armor.CombatArmorItem;
-import com.mojang.blaze3d.systems.RenderSystem;
+import net.minecraft.client.Camera;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Mth;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.client.gui.overlay.IGuiOverlay;
-
-import java.util.Arrays;
 
 public class HelmetOverlay {
     private static long lastTick = -1;
@@ -21,34 +18,39 @@ public class HelmetOverlay {
     private static double diffY = 0;
     private static float xHeadRot = 0;
     private static float yHeadRot = 0;
-    private static final int angle = 10;
+    private static final int angle = 45;
+    private static final int offset = 32;
     public static final IGuiOverlay HUD_HELMET = (gui, guiGraphics, partialTick, screenWidth, screenHeight) -> {
         if (ClientConfig.ENABLE_OVERLAY.get() && Minecraft.getInstance().options.getCameraType().isFirstPerson()) {
-            LocalPlayer player = Minecraft.getInstance().player;
+            Minecraft minecraft = Minecraft.getInstance();
+            LocalPlayer player = minecraft.player;
+            Camera camera = minecraft.gameRenderer.getMainCamera();
             assert player != null;
             ItemStack itemstack = player.getInventory().getArmor(3);
             if (itemstack.getItem() instanceof CombatArmorItem) {
                 String loc = String.format("textures/misc/overlay/%s.png", ClientConfig.OVERLAY_TEXTURE.get());
                 ResourceLocation HELMET_HUD = new ResourceLocation(TaczArmors.MODID, loc);
                 gui.setupOverlayRenderState(true, false);
-                double intensity = ClientConfig.SHAKE_INTENSITY.get();
+                double intensity = ClientConfig.SHAKE_SENSITIVITY.get();
+
                 if(intensity != 0.0D) {
-                    assert Minecraft.getInstance().level != null;
+                    assert minecraft.level != null;
                     //if paused
-                    if (lastTick == -1 || Minecraft.getInstance().level.getGameTime() - lastTick > 1) {
-                        xHeadRot = player.getXRot();
-                        yHeadRot = player.yHeadRot;
+                    if (lastTick == -1 || minecraft.level.getGameTime() - lastTick > 1) {
+                        xHeadRot = camera.getXRot();
+                        yHeadRot = camera.getYRot();
                     }
-                    lastTick = Minecraft.getInstance().level.getGameTime();
-                    float changedX = player.getYHeadRot() - yHeadRot;
-                    float changedY = player.getXRot() - xHeadRot;
-                    diffX = Mth.clamp(diffX + changedX, -angle, angle)*intensity;
-                    diffY = Mth.clamp(diffY + changedY, -angle, angle)*intensity;
-                    xHeadRot = player.getXRot();
-                    yHeadRot = player.yHeadRotO;
+                    lastTick = minecraft.level.getGameTime();
+                    float changedX = camera.getYRot() - yHeadRot;
+                    float changedY = camera.getXRot() - xHeadRot;
+                    diffX = Mth.clampedMap(diffX + changedX, -angle, angle, -offset, offset)*intensity;
+                    diffY = Mth.clampedMap(diffY + changedY, -angle, angle, -offset, offset)*intensity;
+                    xHeadRot = camera.getXRot();
+                    yHeadRot = camera.getYRot();
                     /*guiGraphics.drawString(gui.getFont(), "Angles Per Tick: " + diffX, 8, 8, 0xFFFFFF);
                     guiGraphics.drawString(gui.getFont(), "Radians Per Tick: " + Mth.DEG_TO_RAD*diffX, 8, 16, 0xFFFFFF);*/
                 }
+
                 blitHud(guiGraphics, HELMET_HUD, (int) diffX, (int) diffY, screenWidth, screenHeight);
                 diffX = division(diffX);
                 diffY = division(diffY);
@@ -56,16 +58,11 @@ public class HelmetOverlay {
         }
     };
 
-    private static void blitHud(GuiGraphics guiGraphics, ResourceLocation hud, int x, int y, int screenWidth, int screenHeight){
-        guiGraphics.blit(hud, -x - HelmetOverlay.angle, -y - HelmetOverlay.angle, -90, 0, 0, screenWidth + 2* angle, screenHeight + 2* angle, screenWidth + 2* angle, screenHeight + 2* angle);
+    private static void blitHud(GuiGraphics guiGraphics, ResourceLocation hud, int x, int y, int screenWidth, int screenHeight ){
+        guiGraphics.blit(hud, -x - offset, -y - offset, -90, 0, 0, screenWidth + 2* offset, screenHeight + 2* offset, screenWidth + 2* offset, screenHeight + 2* offset);
     }
 
-    private static double division(double divide){
-        return Math.abs(divide) < 0.05D ? 0: divide / 1.1D;
-    }
-
-    private static float powerLerp(float delta, float start){
-        float sign = Mth.sign(delta);
-        return Mth.lerp(Mth.sign(delta)-delta/start, Mth.sign(delta)*start, 0);
+    private static double division(double divide) {
+        return Math.abs(divide) < 0.05D ? 0: divide * ClientConfig.SHAKE_SENSITIVITY.get();
     }
 }
--- src/main/java/com/korobeinik/taczarmors/config/ClientConfig.java
+++ src/main/java/com/korobeinik/taczarmors/config/ClientConfig.java
@@ -1,15 +1,9 @@
 package com.korobeinik.taczarmors.config;
 
-import com.electronwill.nightconfig.core.Config;
 import com.korobeinik.taczarmors.TaczArmors;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.fml.common.Mod;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import java.util.function.Predicate;
 
 @Mod.EventBusSubscriber(modid = TaczArmors.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
 public class ClientConfig {
@@ -23,9 +17,13 @@ public class ClientConfig {
             .comment("What HUD will be used as overlay")
             .define("block_name", "hud_0");
 
-    public static final ForgeConfigSpec.DoubleValue SHAKE_INTENSITY = BUILDER
-            .comment("How intense should the HUD shake be.\nSet 0 to disable shaking")
-            .defineInRange("shake_intensity", 0.5D, 0.0D, 1.0D);
+    public static final ForgeConfigSpec.DoubleValue SHAKE_SENSITIVITY = BUILDER
+            .comment("How sensitive should the HUD shake be.\nSet 0 to disable shaking")
+            .defineInRange("shake_sensitivity", 1.0D, 0.0D, 1.0D);
+
+    public static final ForgeConfigSpec.DoubleValue SHAKE_STIFFNESS = BUILDER
+            .comment("How stiff should the HUD be. The smaller the value, the faster the hud will return to place.")
+            .defineInRange("shake_stiffness", 0.9D, 0.01D, 0.99D);
 
     /*public static final ForgeConfigSpec.ConfigValue<List<? extends Integer>> CUSTOM_COLORS = BUILDER
             .comment("Add custom colors you want to keep in the colors tab")
